41efab0570745a54c8b0eff8a83b91683fe3311d
Thibaut Dutartre <t.dutartre@gmail.com>
2014-07-04 11:44:17 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var livereload = require('gulp-livereload');
+var lr = require('tiny-lr');
+var server = lr();
+
+var svgmin = require('gulp-svgmin');
+var svgSymbols = require('gulp-svg-symbols');
+
+gulp.task('styles', function() {
+    return gulp.src('src/styles/main.less')
+        .pipe(less())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('style'))
+        .pipe(livereload(server))
+        .pipe(notify({ message: 'Style compiled & reloaded' }));
+});
+
+gulp.task('svg-sprites', function() {
+    return gulp.src('src/images/sprites/*.svg')
+        // .pipe(svgmin())
+        .pipe(svgSymbols())
+        .pipe(gulp.dest('images'))
+        .pipe(livereload(server))
+        .pipe(notify({ message: 'SVG compiled & reloaded' }));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/scripts/*.js')
+        .pipe(concat('main.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('scripts'))
+        .pipe(livereload(server))
+        .pipe(notify({ message: 'Script compiled & reloaded' }));
+});
+
+gulp.task('html', function() {
+    return gulp.src('*.html')
+        .pipe(livereload(server))
+        .pipe(notify({ message: 'Page reloaded' }));
+});
+
+gulp.task('watch', function() {
+    server.listen(35729, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        gulp.watch('src/styles/**/main.less', ['styles']);
+        gulp.watch('src/images/sprites/*.svg', ['svg-sprites']);
+        gulp.watch('src/scripts/**/*.js', ['scripts']);
+        gulp.watch('*.html', ['html']);
+    });
+});
+
+gulp.task('default', ['watch']);
---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+function initialize() {
+	$('.links svg').on('click', function(event) {
+		if ($(event.delegateTarget).attr('goto') === 'vsco') {
+			window.open('http://thibaut.vsco.co/?utm_source=user_grid&utm_medium=user_website&utm_campaign=link_to_grid');
+		} else if ($(event.delegateTarget).attr('goto') === 'twitter') {
+			window.open('http://twitter.com/thib_thib');
+		} else if ($(event.delegateTarget).attr('goto') === 'github') {
+			window.open('https://github.com/thibthib');
+		}
+	});
+
+	$('.infos').on('mouseenter mouseleave', function(event) {
+		$('.logo').toggleClass('photo');
+	});
+}
+
+document.addEventListener("DOMContentLoaded", initialize, false);
